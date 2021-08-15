21c06ca1eb45afd0b98547cbb41eacf114979d2d
EmnaNaili <72765098+EmnaNaili@users.noreply.github.com>
2021-08-15 10:25:02 +0100

    first commit

---
+++ src/Components/Profile/Address.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Address = () => {
+ return (
+   <>
+   <h3 style={{ color:"#fdee00"}}>Riadh Landalos, Ariana</h3>
+   </>
+ );
+};
+export default  Address;
---
+++ src/Components/Profile/FullName.js
@@ -0,0 +1,9 @@
+import React from "react";
+const FullName = () => {
+ return (
+   <>
+   <h2 style={{ color:"#fdee00"}}> Emna Naili</h2>
+   </>
+ );
+};
+export default  FullName;
